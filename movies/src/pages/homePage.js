19d0e58280f9fa7e1da35fcac2fd4d69edf53c6f
Tara Mccarthy <mccarthytara14@gmail.com>
2023-04-09 16:12:51 +0100

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -8,6 +8,32 @@ import Grid from "@mui/material/Grid";
 const MovieListPage = (props) => {
     const [movies, setMovies] = useState([]);
 
+    const [nameFilter, setNameFilter] = useState("");
+    const [genreFilter, setGenreFilter] = useState("0");
+
+    const genreId = Number(genreFilter);
+
+    let displayedMovies = movies
+      .filter((m) => {
+        return m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
+      })
+      .filter((m) => {
+        return genreId > 0 ? m.genre_ids.includes(genreId) : true;
+      });
+  
+    const handleChange = (type, value) => {
+      if (type === "name") setNameFilter(value);
+      else setGenreFilter(value);
+    };
+  
+    const addToFavorites = (movieId) => {
+        const updatedMovies = movies.map((m) =>
+          m.id === movieId ? { ...m, favorite: true } : m
+        );
+        setMovies(updatedMovies);
+      };
+    
+
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -30,9 +56,16 @@ const MovieListPage = (props) => {
       </Grid>
       <Grid item container spacing={5}>
         <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
-          <FilterCard />
+        <FilterCard
+      onUserInput={handleChange}
+      titleFilter={nameFilter}
+      genreFilter={genreFilter}
+    />
+
         </Grid>
-        <MovieList movies={movies}></MovieList>
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
+
+
       </Grid>
     </Grid>
   );
