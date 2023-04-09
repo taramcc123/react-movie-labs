bdcf25b5ad93fc5c0e06f05dff3a2fbf2d4a4ed5
Tara Mccarthy <mccarthytara14@gmail.com>
2023-04-09 17:34:16 +0100

    Show movie review excerpts; Refactor Details page.

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -30,3 +30,15 @@ export const getMovies = () => {
       .then((json) => json.posters);
   };
   
+
+  export const getMovieReviews = (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        // console.log(json.results);
+        return json.results;
+      });
+  };
+  
