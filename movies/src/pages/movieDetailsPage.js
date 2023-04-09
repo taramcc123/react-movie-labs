f08db73e62968fc5818ef0049a3db1d48a167266
Tara Mccarthy <mccarthytara14@gmail.com>
2023-04-09 16:23:41 +0100

    Refactor: Template list page; HTTP API calls centralized.

--- movies/src/pages/movieDetailsPage.js
+++ movies/src/pages/movieDetailsPage.js
@@ -6,6 +6,7 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import React, {useState, useEffect}  from "react";
 import { useParams } from 'react-router-dom';
+import { getMovie, getMovieImages } from "../api/tmdb-api";
 
 
 
@@ -15,31 +16,18 @@ const MoviePage = (props) => {
     const [images, setImages] = useState([]);
   
     useEffect(() => {
-      fetch(
-        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-      )
-        .then((res) => {
-          return res.json();
-        })
-        .then((movie) => {
-          // console.log(movie)
+        getMovie(id).then((movie) => {
           setMovie(movie);
         });
-    }, [id]);
-  
-    useEffect(() => {
-      fetch(
-        `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-      )
-        .then((res) => res.json())
-        .then((json) => json.posters)
-        .then((images) => {
-          // console.log(images)
+      }, [id]);
+    
+      useEffect(() => {
+        getMovieImages(id).then((images) => {
           setImages(images);
         });
-        // eslint-disable-next-line
-    }, []);
-  
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, []);
+    
 
   return (
     <>
